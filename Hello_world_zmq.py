021f220ab139d26cfed84519b9b7f02a301913bf
zsfVishnu <34836841+zsfVishnu@users.noreply.github.com>
2019-01-24 17:12:42 +0530

    Update Hello_world_zmq.py
    Images yet to be received

--- Hello_world_zmq.py
+++ Hello_world_zmq.py
@@ -7,6 +7,9 @@
 
 import time
 import zmq
+import json
+
+
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
@@ -15,9 +18,13 @@ socket.bind("tcp://*:5555")
 while True:
 	#  Wait for next request from client
 	message = socket.recv()
-	print("Request Recieved : %s" %message)
-	#Do some work
+	#print(message.decode('utf-8'))
+	#print(json.loads(message.decode('utf-8')))
+	print(repr(message.decode('utf-8')))	
+#Do some work
 	time.sleep(1)
-
+	
 	#send reply back to client
 	socket.send(b"World")
+
+
