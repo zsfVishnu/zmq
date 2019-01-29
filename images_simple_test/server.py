f9fc26789e23d957c015528302af897548745dbc
zsfVishnu <34836841+zsfVishnu@users.noreply.github.com>
2019-01-29 18:52:34 +0530

    Final changes made

--- images_simple_test/server.py
+++ images_simple_test/server.py
@@ -1,41 +1,26 @@
-#
-#   Hello World server in Python
-#   Binds REP socket to tcp://*:5555
-#   Expects b"Hello" from client, replies with b"World
-#SERVER 
-#code inspired from : http://zguide.zeromq.org/py:hwserver
-
-import time
 import zmq
-import base64
+import json
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
-#socket.setsockopt(zmq.SUBSCRIBE, b'')
-
 
 while True:
-	#  Wait for next request from client
-	#message = socket.recv()
-	#print("Request Recieved : %s" %message)
-	#Do some work
-	frame = socket.recv()
-	#print("Request Recieved : %s" %frame)
-	#img = base64.b64decode(frame)
-	npimg = np.fromstring(frame, dtype=np.uint8)
-	source = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-	print (type(frame))
-	print (type(npimg))
-	print (type(source))
-	cv2.imshow("image", source)
-	cv2.waitKey(0)
-	#time.sleep(1)
+	json_str = socket.recv()
+	print("no error till here")
+	data_print = json.loads(json_str)
+	print(data_print["Image"]["rows"],data_print["Image"]["cols"])  #Check the number of rows and columns
 	
+	img = np.array(data_print["Image"]["data"])
+	img = img.reshape(data_print["Image"]["rows"],data_print["Image"]["cols"], data_print["Image"]["channels"])
+	
+	b,g,r = cv2.split(img)      #Image channels will be inverted ie BGR 
+	img = cv2.merge((r,g,b))
+	
+	plt.imshow(img)
+	plt.show()
 
-	#send reply back to client
-	socket.send(b"World")
-
-socket.close();
+	socket.send(b"Hello")
